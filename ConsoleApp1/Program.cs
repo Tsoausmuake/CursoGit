a95cae49a31964e52c7bd40243de9346fd58c572
Manoel Jorge <manoel.jmb1990@gmail.com>
2020-08-28 22:54:50 -0300

    testando

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,10 @@ namespace ConsoleApp1
             Console.WriteLine("Olá mundo");
             Console.WriteLine("Olá mundo2");
             Console.WriteLine("Olá mundo5");
-            
 
+            int i = 50;
+
+            Console.WriteLine(i);
            
         }
     }
